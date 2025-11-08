2ce893caa17145e6157ede963dfa743434e32074
giangdc24v7x309 <giangdc24v7x309@dttx.ctu.edu.vn>
2025-11-08 18:31:24 +0700

    Update app.js

--- contactbook-backend/app.js
+++ contactbook-backend/app.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to contact book application." });
+});
 // handle 404 response
  app.use((req, res, next) => {
  // Code ở đây sẽ chạy khi không có route được định nghĩa nào
@@ -22,8 +26,6 @@ app.use((err, req, res, next) => {
     message: err.message || "Internal Server Error",
   });
 });
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to contact book application." });
-});
+
 
 module.exports = app;
